3d0389910ffe7381a130429a346215eb676c6220
Saurabh Pal <saurabhpal.jhs@gmail.com>
2026-06-30 11:10:57 +0530

    Refactor comments in eks.tf for better understanding
    Updated comments for clarity and consistency.

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,21 +2,22 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "ramji-atm-cluster" # Hamari Jenkinsfile ka absolute cluster identification name
+  cluster_name    = "ramji-atm-cluster"
   cluster_version = "1.30"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  cluster_endpoint_public_access = true # Jenkins container dynamic command handling access ke liye true
+  # Required for Jenkins master/agent connection to control the cluster
+  cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
     ramji_atm_nodes = {
       min_size     = 1
       max_size     = 3
-      desired_size = 2 # At least 2 servers hamesha replication ke liye ready rahenge
+      desired_size = 2
 
-      instance_types = ["t3.medium"] # ATM logic processing aur stability ke liye right buffer capacity
+      instance_types = ["t3.medium"]
     }
   }
 }
